4c1862e2ca2915a0366f389f04506b44701989a1
hetjjj <30998335+hetjjj@users.noreply.github.com>
2020-06-22 02:38:03 -0400

    Updates

---
+++ precache-manifest.f42c804571867890183c5dd216d18834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd75c9773be46062563df54fb4db6b1",
+    "url": "/App/index.html"
+  },
+  {
+    "revision": "463140bcc7ce2323e5f7",
+    "url": "/App/static/css/main.f392cf93.chunk.css"
+  },
+  {
+    "revision": "b658d3b21ac04c605c80",
+    "url": "/App/static/js/2.cbae7900.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/App/static/js/2.cbae7900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463140bcc7ce2323e5f7",
+    "url": "/App/static/js/main.dc3a81af.chunk.js"
+  },
+  {
+    "revision": "2aa29c90553cee17ab83",
+    "url": "/App/static/js/runtime-main.ddc0b344.js"
+  }
+]);
--- src/util/cardMappings.js
+++
@@ -1,6 +0,0 @@
-export const cardMappings = {
-    "ACE": [1, 11],
-    "KING": 10,
-    "QUEEN": 10,
-    "JACK": 10
-}
